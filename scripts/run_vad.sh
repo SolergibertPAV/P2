e00a90d3c4c4aadec859a43e279e79036b91c599
Antoni-Joan Solergibert i Llaquet <antoni-joan.solergibert@estudiantat.upc.edu>
2021-03-27 22:45:12 +0100

    Todo en orden. A dormir

--- scripts/run_vad.sh
+++ scripts/run_vad.sh
@@ -2,17 +2,17 @@
 
 # Be sure that this file has execution permissions:
 # Use the nautilus explorer or chmod +x run_vad.sh
-for num_init in 9 10; do
-#for num_ms in 1 2 3 4 5 6; do
-#for num_mv in 1 2 3 4 5 6; do  
-#for alpha_uno in 1 2 3 4; do
-#for alpha_dos in 1 2 3 4; do
+for num_init in 6 7 8 9 10 11 12 13 14 15 16; do
+for num_ms in 5 10 15 20 25 30; do
+for num_mv in 5 10 15 20 25 30; do  
+for alpha_uno in 2 3 4 5; do
+for alpha_dos in 1 2 3 4 5 6 7 8 9; do
 # -N <number-init> -s <number-ms> -m <number-mv> -a <n-alpha1> -b <n-alpha2>
 # Write here the name and path of your program and database
 DB=/Users/tj/Desktop/PAV/P2/db.v4
-#CMD="bin/vad -N $num_init -s $num_ms -m $num_mv -a $alpha_uno -b $alpha_dos"
-CMD="bin/vad -N $num_init"
-
+CMD="bin/vad -N $num_init -s $num_ms -m $num_mv -a $alpha_uno -b $alpha_dos"
+# CMD="bin/vad -N $num_init"
+echo Parametros: Number init: $num_init, Number MS: $num_ms, Number MV: $num_mv, Alpha1: $alpha_uno, Alpha2: $alpha_dos
 for filewav in $DB/*/*wav; do
 #    echo
     echo "**************** $filewav ****************"
@@ -33,8 +33,8 @@ done
 
 scripts/vad_evaluation.pl $DB/*/*lab
 done
-#done
-#done
-#done
-#done
+done
+done
+done
+done
 exit 0
