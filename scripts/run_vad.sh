7fdd6d289ec318425c018b0a8600123c3274bd6d
Antoni-Joan Solergibert i Llaquet <antoni-joan.solergibert@estudiantat.upc.edu>
2021-03-27 20:48:32 +0100

    Funciona perfecta con dos argumentos

--- scripts/run_vad.sh
+++ scripts/run_vad.sh
@@ -2,10 +2,10 @@
 
 # Be sure that this file has execution permissions:
 # Use the nautilus explorer or chmod +x run_vad.sh
-for num_init in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15; do
+for num_init in 9 10; do
 # Write here the name and path of your program and database
 DB=/Users/tj/Desktop/PAV/P2/db.v4
-CMD="bin/vad -N $num_init"
+CMD="bin/vad -N $num_init -a 2"
 
 for filewav in $DB/*/*wav; do
 #    echo
